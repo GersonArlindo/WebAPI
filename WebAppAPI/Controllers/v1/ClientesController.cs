97130ea41b18c000f7d5a874a72d7cfab6896831
Gerson Arlindo <gerson50039@gmail.com>
2021-05-30 21:11:08 -0600

    Agregando Delete y Update a Cliente

--- WebAppAPI/Controllers/v1/ClientesController.cs
+++ WebAppAPI/Controllers/v1/ClientesController.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Feauters.Clientes.Commands.CreateClienteCommand;
+using Application.Feauters.Clientes.Commands.DeleteClienteCommand;
+using Application.Feauters.Clientes.Commands.UpdateClienteCommand;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -16,6 +18,24 @@ namespace WebAppAPI.Controllers.v1
         public async Task<IActionResult> Post(CreateClienteCommand command)
         {
             return Ok(await Mediator.Send(command));
+        }
+
+        //Put api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, UpdateClienteCommand command)
+        {
+            if (id != command.Id)
+
+                return BadRequest();
+            
+            return Ok(await Mediator.Send(command));
+        }
+
+        //Delete api/<controller>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await Mediator.Send(new DeleteClienteCommand { Id = id}));
         }
     }
 }
--- WebAppAPI/Startup.cs
+++ WebAppAPI/Startup.cs
@@ -34,6 +34,7 @@ namespace WebAppAPI
             services.AddSharedInfraestructure(Configuration);
             services.AddPersistenceInfraestructure(Configuration);
             services.AddControllers();
+            services.AddApiVersioningExtension();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAppAPI", Version = "v1" });
