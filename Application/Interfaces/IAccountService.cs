68dc68cd47688997c2e19f2318f3c92b8f9011f0
Lud12 <ludwinportillo_1999@hotmail.com>
2021-06-10 14:25:25 -0600

    Creando servicio autenticador e implementando Refresh Token

---
+++ Application/Interfaces/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.Users;
+using Application.Wrappers;
+
+namespace Application.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<Response<AuthenticationResponse>> AuthenticationAsync(AuthenticationRequest request, string ipAddress);
+        Task<Response<string>> RegisterAsync(RegisterRequest request, string origin);
+    }
+}
