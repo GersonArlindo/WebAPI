09c765280aee63fd7e885bfbb3e50e2f9ad8aaae
abigailcruz <jhojairamarquez.99@gmail.com>
2021-06-12 20:47:34 -0600

    Protegiendo Endpoints con Authorize

---
+++ Identity/Seeds/DefaultAdminUser.cs
@@ -0,0 +1,41 @@
+﻿using Application.Enums;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Seeds
+{
+    public static class DefaultAdminUser
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            // Seed Default AdminUser
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "userAdmin",
+                Email = "userAdmin@gmail.com",
+                Nombre = "Noel",
+                Apellido = "Canales",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+
+            };
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123Pa$word");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Basic.ToString());
+
+                }
+            }
+        }
+    }
+}
